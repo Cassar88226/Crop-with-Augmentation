97b610e5ec8af6faff584b7aa164a7d3541bbc1f
Vadim88226 <kingdragon2108@gmail.com>
2021-12-08 11:22:09 +0300

    add contrast, gaussian distortion, brightness, skew

--- 1.py
+++ 1.py
@@ -28,6 +28,26 @@ for x in range(count):
     shearamount = random.randint(-10,10)
     sheardir = random.randint(0,1)
     p1.shear(probability=1, max_shear_left=shearamount, max_shear_right=sheardir)
+    
+    # random contrast
+    p1.random_contrast(probability=1, min_factor = 0, max_factor = 1)
+    
+    # gaussian distortion
+    grid_width = random.randint(2, 10)
+    grid_height = random.randint(2, 10)
+    magnitude = random.randint(1, 10)
+    p1.gaussian_distortion(probability=1, grid_width=grid_width, grid_height=grid_height, magnitude=magnitude, corner='bell', method="in")
+    
+    # random brightness
+    p1.random_brightness(probability=1, min_factor = 0, max_factor = 1)
+    
+    # skew
+    skewmagnitude = random.uniform(0.1, 1)
+    p1.skew(probability=1, magnitude=skewmagnitude)
+    
+    # random erase
+    # erasingfactor = random.uniform(0.1, 1)
+    # p1.random_erasing(probability=1, rectangle_area = erasingfactor)
     p1.sample(count = x, multi_threaded = False)
 
     p2 = Augmentor.Pipeline("./2")
@@ -37,6 +57,10 @@ for x in range(count):
     p2.zoom(probability=1, min_factor=zoomfactor, max_factor=1.5)
     p2.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=distomagnitude)
     p2.shear(probability=1, max_shear_left=shearamount, max_shear_right=sheardir)
+    p2.random_contrast(probability=1, min_factor = 0, max_factor = 1)
+    p2.gaussian_distortion(probability=1, grid_width=grid_width, grid_height=grid_height, magnitude=magnitude, corner='bell', method="in")
+    p2.random_brightness(probability=1, min_factor = 0, max_factor = 1)
+    p2.skew(probability=1, magnitude=skewmagnitude)
     p2.sample(count = x, multi_threaded=False)
 
     p3 = Augmentor.Pipeline("./3")
@@ -46,6 +70,10 @@ for x in range(count):
     p3.zoom(probability=1, min_factor=zoomfactor, max_factor=1.5)
     p3.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=distomagnitude)
     p3.shear(probability=1, max_shear_left=shearamount, max_shear_right=sheardir)
+    p3.random_contrast(probability=1, min_factor = 0, max_factor = 1)
+    p3.gaussian_distortion(probability=1, grid_width=grid_width, grid_height=grid_height, magnitude=magnitude, corner='bell', method="in")
+    p3.random_brightness(probability=1, min_factor = 0, max_factor = 1)
+    p3.skew(probability=1, magnitude=skewmagnitude)
     p3.sample(count = x, multi_threaded=False)
 
     p4 = Augmentor.Pipeline("./4")
@@ -55,4 +83,8 @@ for x in range(count):
     p4.zoom(probability=1, min_factor=zoomfactor, max_factor=1.5)
     p4.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=distomagnitude)
     p4.shear(probability=1, max_shear_left=shearamount, max_shear_right=sheardir)
+    p4.random_contrast(probability=1, min_factor = 0, max_factor = 1)
+    p4.gaussian_distortion(probability=1, grid_width=grid_width, grid_height=grid_height, magnitude=magnitude, corner='bell', method="in")
+    p4.random_brightness(probability=1, min_factor = 0, max_factor = 1)
+    p4.skew(probability=1, magnitude=skewmagnitude)
     p4.sample(count=x, multi_threaded=False)
